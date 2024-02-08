69257213c20b4e0550fa0c09b8b05cb0196b6bd9
kushal-iliger <k.jagadeesh-iliger@hellogetsafe.com>
2024-02-08 14:43:47 +0100

    Herosection is complete

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    // Add the following rule to handle .gz files
+    config.module.rules.push({
+      test: /\.gz$/,
+      use: 'gzip-loader',
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
+
